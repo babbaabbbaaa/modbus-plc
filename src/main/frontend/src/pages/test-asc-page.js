a24f93f9fa29a5a0359fa90be9454f66accb3d80
leonali2 <leonali2@cisco.com>
2022-02-17 09:00:31 +0800

    change style

--- src/main/frontend/src/pages/test-asc-page.js
+++ src/main/frontend/src/pages/test-asc-page.js
@@ -19,40 +19,28 @@ class TestAscPage extends React.Component {
 					controlType: 'Input',
 					key: 'logTime',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '模具号(D7004)',
 					controlType: 'Input',
 					key: 'moldNo',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '压射号(D7005)',
 					controlType: 'Input',
 					key: 'injectionNo',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
                 {
                     label: '产品类型(D7003)',
                     controlType: 'Input',
                     key: 'productTypeId',
                     disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-                    labelCol: {
-                        span: 9
-                    }
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
                 }
 			]
 		},
@@ -65,130 +53,91 @@ class TestAscPage extends React.Component {
 					controlType: 'Input',
 					key: 'barcodeData',
 					disabled: true,
-					col: {xs:24, sm:24,md:24,lg:24,xl:24},
-					labelCol: {
-						span: 6
-					}
+					col: {xs:24, sm:24,md:24,lg:24,xl:24}
 				},
 				{
 					label: '流水号(D7007)',
 					controlType: 'Input',
 					key: 'serialNumber',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
                 {
 					label: '二维码截取',
 					controlType: 'Input',
 					key: 'barcode',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
                 {
 					label: '二维码等级',
 					controlType: 'Input',
 					key: 'barcodeGrade',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '读码判定结果(D7009)',
 					controlType: 'Input',
 					key: 'codeReadingFunc',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重差值(D7018/D7019)',
 					controlType: 'Input',
 					key: 'weightDifference',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重判定结果',
 					controlType: 'Input',
 					key: 'weightResult',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重公差上限(D7020/D7021)',
 					controlType: 'Input',
 					key: 'maxWeightTolerance',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重公差下限(D7022/D7023)',
 					controlType: 'Input',
 					key: 'minWeightTolerance',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重压铸前(D7014/D7015)',
 					controlType: 'Input',
 					key: 'weightBeforeDieCasting',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重压铸后(D7016/D7017)',
 					controlType: 'Input',
 					key: 'weightAfterDieCasting',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '使用产品/穴数选择(D7006)',
 					controlType: 'Input',
 					key: 'holeSelection',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '打标使用状态(D7008)',
 					controlType: 'Input',
 					key: 'codeReadingFunc',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				}
 			]
 		},
@@ -201,120 +150,84 @@ class TestAscPage extends React.Component {
 					controlType: 'Input',
 					key: 'barcodeData',
 					disabled: true,
-					col: {xs:24, sm:24,md:24,lg:24,xl:24},
-					labelCol: {
-						span: 6
-					}
+					col: {xs:24, sm:24,md:24,lg:24,xl:24}
 				},
 				{
 					label: '流水号(D7011)',
 					controlType: 'Input',
 					key: 'serialNumber',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 10
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
                 {
 					label: '二维码截取',
 					controlType: 'Input',
 					key: 'barcode',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
                 {
 					label: '二维码等级',
 					controlType: 'Input',
 					key: 'barcodeGrade',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '读码判定结果(D7013)',
 					controlType: 'Input',
 					key: 'codeReadingFunc',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重差值(D7018/D7019)',
 					controlType: 'Input',
 					key: 'weightDifference',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重判定结果',
 					controlType: 'Input',
 					key: 'weightResult',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重公差上限(D7030/D7031)',
 					controlType: 'Input',
 					key: 'maxWeightTolerance',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重公差下限(D7032/D7033)',
 					controlType: 'Input',
 					key: 'minWeightTolerance',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重压铸前(D7024/D7025)',
 					controlType: 'Input',
 					key: 'weightBeforeDieCasting',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '铝重压铸后(D7026/D7027)',
 					controlType: 'Input',
 					key: 'weightAfterDieCasting',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '使用产品/穴数选择(D7010)',
 					controlType: 'Input',
 					key: 'holeSelection',
 					disabled: true,
-					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
+					col: {xs:24, sm:24,md:12,lg:12,xl:12}
 				},
 				{
 					label: '打标使用状态(D7012)',
@@ -322,9 +235,6 @@ class TestAscPage extends React.Component {
 					key: 'codeReadingFunc',
 					disabled: true,
 					col: {xs:24, sm:24,md:12,lg:12,xl:12},
-					labelCol: {
-						span: 9
-					}
 				}
 			]
 		}
@@ -355,7 +265,7 @@ class TestAscPage extends React.Component {
 			<Button className = "btn submit-btn" onClick = { this.testHandle } > 测试 </Button> 
 			{
 				this.testFormList.map(item => {
-					return <div className='test-form-item'>
+					return <div className='test-form-item' >
 						<div className='test-form-item-name'>{item.name}</div>
 						<Form name = 'search' ref = { item.formRef } >
 							<Row >
