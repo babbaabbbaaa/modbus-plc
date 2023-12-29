80c8fde031a11f0a16802e0cd38ad058012d9c17
Rocker Chen <rocker.chen@outlook.com>
2023-12-29 16:17:51 +0800

    start to upgrade jdk from 1.8 to 17

--- src/main/java/com/demo/authentication/User.java
+++ src/main/java/com/demo/authentication/User.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.Collection;
 import java.util.Set;
 
